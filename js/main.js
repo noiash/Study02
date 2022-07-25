2f5da0ef0423fa8257192a53ca70328c931e1a0b
NOI <88365739+noiash@users.noreply.github.com>
2022-07-25 10:49:23 +0900

    [Start]JavaScript 基礎文法編

---
+++ js/main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log('hello');
+console.log('hello');
+
+// console.log('it's me!);
+// console.log("it's me!");
+// console.log('it\'s me!');
+
+console.log('hel\nlo wor\tld');
+
+
+// コメント
+
+/*
+コメント
+コメント
+*/
+
+// console.log("Hello World from main.js!");
+
+
