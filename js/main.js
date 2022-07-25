c59750481a2274c0df28e2f911c6579e5c0730e1
NOI <88365739+noiash@users.noreply.github.com>
2022-07-25 18:24:22 +0900

    [Add]JavaScript 基礎文法編

--- js/main.js
+++ js/main.js
@@ -1,10 +1,59 @@
 "use strict";
 
-console.log(typeof 'hello');
-console.log(typeof 5);
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(typeof null);
+const score = 60;
+const name = 'taguchi';
+
+if(score >= 50) {
+  if(name === 'taguchi') {
+    console.log('Good job!');
+  }
+}
+ 
+if (score >= 50 && name === 'taguchi') {
+  console.log('Good job!');
+}
+
+//  && なおかつ (AND)
+//  ||　もしくは (OR)
+//  ! 　〜ではない (NOT)
+
+
+// if(score >= 80) {
+//   console.log('Great!');
+// } else {
+//   console.log('OK...');
+// }
+
+// score >= 80 ? console.log('Great!') : console.log('OK...');
+
+
+// const price = 1200;
+
+// console.log(price > 1000);  //true
+// console.log(price < 1000);  //false
+// console.log(price >= 1000);  //true
+// console.log(price <= 1000);  //false
+// console.log(price === 1000);  //false
+// console.log(price !== 1000);  //true
+
+// false <- 0, null, undefined, '', false
+// true それ以外
+
+// console.log(Boolean(0));
+// console.log(Boolean('hello'));
+// console.log('5' * 3);
+// console.log('5' - 3);
+
+// console.log('5' + 3);
+// console.log(parseInt('5', 10) + 3);
+
+// console.log(parseInt('hello', 10));
+
+// console.log(typeof 'hello');
+// console.log(typeof 5);
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof null);
 // let price = 500;
 
 // price = price + 100;
