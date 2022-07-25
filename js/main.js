030a20c6cdcd16cf74af9f79cc67096cc167d13a
NOI <88365739+noiash@users.noreply.github.com>
2022-07-25 19:13:29 +0900

    [Finish]JavaScript 基礎文法編

--- js/main.js
+++ js/main.js
@@ -1,17 +1,129 @@
 "use strict";
 
-const score = 60;
-const name = 'taguchi';
+{
+  const x = 100;
+  console.log(x);
 
-if(score >= 50) {
-  if(name === 'taguchi') {
-    console.log('Good job!');
-  }
 }
+
+
+// const x = 2;
+
+// function f() {
+//   // const x = 1;
+//   console.log(x);
+// }
+
+// f();
+// console.log(x);
+
+// function sum(a, b, c) {
+  // console.log(a + b + c);
+  // return a + b + c;
+// }
+
+// const sum = function(a, b, c) {
+//   return a + b + c;
+// };
+
+// const double = function(a) {
+//   return a * 2;
+// }
+
+// const double = a => a * 2;
+// console.log(double(12));
+
+// const sum = (a, b, c) => a + b + c;
+
+// sum(1, 2, 3);
+// sum(3, 4, 5);
+
+// const total = sum(1, 2, 3) + sum(3, 4, 5); //18
+// console.log(total);
+
+// function showAd(message = 'Ad') { //仮引数
+//   console.log('------------');
+//   console.log(`-----${message}-----`);
+//   console.log('------------');
+// }
+
+// showAd('Header Ad');　//実引数
+// console.log('Tom is great!');
+// console.log('Bob is great!');
+// showAd();
+// console.log('Steve is great!');
+// console.log('Richard is great!');
+// showAd('Footer Ad');
+
+// for (let i = 1; i <= 10; i++) {
+  // if (i === 4) {
+    //  if (i % 3 === 0) {
+    //    continue;
+    //   }
+//     if (i === 4) {
+//       break;
+//     }
+//   console.log(i);
+// }
+
+// let hp = 100;
+
+// while (hp > 0) {
+//   console.log(`${hp} HP left!`);
+//   hp -= 15;
+// }
+
+// let hp = -50;
+
+// do {
+//   console.log(`${hp} HP left!`);
+//   hp -= 15;
+// } while (hp > 0);
+
+// for (let i = 1; i <= 10; i++) {
+  // console.log('hello');
+  // console.log('hello' + i);
+  // console.log(`hello ${i}`);
+// }
+
+// const signal = 'pink';
+
+// if(signal === 'red') {
+//   console.log('Stop!');
+// } else if (signal === 'yellow') {
+//   console.log('Caution!');
+// } else if (signal === 'blue') {
+//   console.log('Go!');
+// }
+
+// switch (signal) {
+//   case 'red':
+//     console.log('Stop!');
+//   break;
+//   case 'yellow':
+//     console.log('Caution!');
+//   break;
+//   case 'blue':
+//   case 'green':
+//     console.log('Go!');
+//   break;
+//   default:
+//     console.log('Wrong signal!');
+//     break;
+// }
+
+// const score = 60;
+// const name = 'taguchi';
+
+// if(score >= 50) {
+//   if(name === 'taguchi') {
+//     console.log('Good job!');
+//   }
+// }
  
-if (score >= 50 && name === 'taguchi') {
-  console.log('Good job!');
-}
+// if (score >= 50 && name === 'taguchi') {
+//   console.log('Good job!');
+// }
 
 //  && なおかつ (AND)
 //  ||　もしくは (OR)
