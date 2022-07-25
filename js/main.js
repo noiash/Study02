1ecce7fdf67253a23772a9c9d7494d495dbcd9fa
NOI <88365739+noiash@users.noreply.github.com>
2022-07-25 11:06:26 +0900

    [Add]JavaScript 基礎文法編

--- js/main.js
+++ js/main.js
@@ -1,22 +1,50 @@
 "use strict";
 
-console.log('hello');
-console.log('hello');
+console.log(typeof 'hello');
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+// let price = 500;
 
-// console.log('it's me!);
-// console.log("it's me!");
-// console.log('it\'s me!');
+// price = price + 100;
+// price += 100; //600
 
-console.log('hel\nlo wor\tld');
+// price = price * 2;
+// price *= 2; //1200
+
+// price = price + 1;
+// price += 1;
+// price++; 1201//
+
+// price = price - 1;
+// price -= 1;
+// price--; //1200
+
+// console.log(price);
 
 
-// コメント
+//定数 const
+//変数 let var
 
-/*
-コメント
-コメント
-*/
+// let price = 150;
+// console.log(price * 140);
+// console.log(price * 160);
 
-// console.log("Hello World from main.js!");
+// price = 170;
+// console.log(price * 140);
+// console.log(price * 160);
+
+// console.log(10 + 3); //13
+// console.log(10 - 3); //7
+// console.log(10 * 3);  //30
+// console.log(10 / 3);  //3.3333...
+// console.log(10 % 3); //1
+// console.log(10 ** 3); //1000
+// console.log(2 + 10 * 3) //32
+// console.log((2 + 10) * 3) //36
+
+
+
 
 
