c0a957b0563417ce3ea2e0e9a826e979ee54a11d
NOI <88365739+noiash@users.noreply.github.com>
2022-07-28 22:57:16 +0900

    [Add]JavaScript Intersection Observer API編

---
+++ MyIntersectionObserver/js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+{
+  const target = document.querySelector('img');
+
+  function callback() {
+    console.log('fired!');
+  }
+
+  const options = {
+    threshold: [0.2, 0.8],
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
+  observer.observe(target);
+}
