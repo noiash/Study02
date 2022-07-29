54c7a9ae69da409bd7578ed03ffef98fb1e2380a
NOI <88365739+noiash@users.noreply.github.com>
2022-07-29 10:50:59 +0900

    [Add]カルーセルを作ろう

---
+++ MyCarousel/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+{
+  const next = document.getElementById('next');
+  const ul = document.querySelector('ul');
+  const slides = ul.children;
+
+  next.addEventListener('click', ()=> {
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    ul.style.transform = `translateX(${-1 * slideWidth}px)`;
+  });
+}
