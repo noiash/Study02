943af5aa0968a3383899630f846d3ddd9d447a21
NOI <88365739+noiash@users.noreply.github.com>
2022-07-29 11:49:31 +0900

    [Finish]カルーセルを作ろう

--- MyCarousel/js/main.js
+++ MyCarousel/js/main.js
@@ -1,6 +1,12 @@
 'use strict';
 
 {
+  const next = document.getElementById('next');
+  const prev = document.getElementById('prev');
+  const ul = document.querySelector('ul');
+  const slides = ul.children;
+  const dots = [];
+  let currentIndex = 0;
 
   function updateButtons() {
     prev.classList.remove('hidden');
@@ -9,7 +15,6 @@
     if (currentIndex === 0) {
       prev.classList.add('hidden');
     }
-
     if (currentIndex === slides.length - 1) {
       next.classList.add('hidden');
     }
@@ -19,24 +24,47 @@
     const slideWidth = slides[0].getBoundingClientRect().width;
     ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
   }
-  
-  const next = document.getElementById('next');
-  const prev = document.getElementById('prev');
-  const ul = document.querySelector('ul');
-  const slides = ul.children;
-  let currentIndex = 0;
-  
-  updateButtons();
 
-  next.addEventListener('click', ()=> {
+  function setupDots() {
+    for (let i = 0; i < slides.length; i++) {
+      const button = document.createElement('button');
+      button.addEventListener('click', () => {
+        currentIndex = i;
+        updateButtons();
+        updateDots();
+        moveSlides();
+      })
+      dots.push(button);
+      document.querySelector('nav').appendChild(button);
+    }
+    dots[0].classList.add('current');
+  }
+
+  function updateDots() {
+    dots.forEach(dot => {
+      dot.classList.remove('current');
+    });
+    dots[currentIndex].classList.add('current');
+  }
+
+  updateButtons();
+  setupDots();
+
+  next.addEventListener('click', () => {
     currentIndex++;
     updateButtons();
+    updateDots();
     moveSlides();
   });
 
-  prev.addEventListener('click', ()=> {
+  prev.addEventListener('click', () => {
     currentIndex--;
     updateButtons();
+    updateDots();
+    moveSlides();
+  });
+
+  window.addEventListener('resize', () => {
     moveSlides();
   });
 }
