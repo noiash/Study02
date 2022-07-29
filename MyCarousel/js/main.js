f87b776724b48f3b7badc210963e53ef150ec4a9
NOI <88365739+noiash@users.noreply.github.com>
2022-07-29 11:02:23 +0900

    [Add]カルーセルを作ろう

--- MyCarousel/js/main.js
+++ MyCarousel/js/main.js
@@ -1,12 +1,42 @@
 'use strict';
 
 {
+
+  function updateButtons() {
+    prev.classList.remove('hidden');
+    next.classList.remove('hidden');
+
+    if (currentIndex === 0) {
+      prev.classList.add('hidden');
+    }
+
+    if (currentIndex === slides.length - 1) {
+      next.classList.add('hidden');
+    }
+  }
+
+  function moveSlides() {
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
+  }
+  
   const next = document.getElementById('next');
+  const prev = document.getElementById('prev');
   const ul = document.querySelector('ul');
   const slides = ul.children;
+  let currentIndex = 0;
+  
+  updateButtons();
 
   next.addEventListener('click', ()=> {
-    const slideWidth = slides[0].getBoundingClientRect().width;
-    ul.style.transform = `translateX(${-1 * slideWidth}px)`;
+    currentIndex++;
+    updateButtons();
+    moveSlides();
+  });
+
+  prev.addEventListener('click', ()=> {
+    currentIndex--;
+    updateButtons();
+    moveSlides();
   });
 }
